f93bc56638c839b577874c8f3e8b7305cfdf9eac
Mihai <lita.mihai@gmail.com>
2023-01-25 23:06:27 +0200

    rebase

---
+++ 1.Sqs/SqsPublisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+using System.Text.Json;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "lita.mihai@gmail.com",
+    FullName = "Mihai Lita",
+    DateOfBirth = new DateTime(1985, 5, 26),
+    GitHubUsername = "atryx"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+
+        "MessageType" , new MessageAttributeValue
+        {
+            DataType = "String",
+            StringValue = nameof(CustomerCreated)
+        }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
